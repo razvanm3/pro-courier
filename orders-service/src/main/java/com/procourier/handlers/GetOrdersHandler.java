0285a9e553b24ad632bcb9b44b070fefcc553a37
Razvan Mihai <42058259+razvanm3@users.noreply.github.com>
2019-04-02 07:18:44 +0300

    Add files via upload

--- orders-service/src/main/java/com/procourier/handlers/GetOrdersHandler.java
+++ orders-service/src/main/java/com/procourier/handlers/GetOrdersHandler.java
@@ -1,4 +1,33 @@
 package com.procourier.handlers;
 
-public class GetOrdersHandler {
+import com.google.gson.Gson;
+import com.procourier.model.Order;
+import com.procourier.model.repository.OrderRepository;
+import service.OrderService;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import java.util.List;
+
+import static java.lang.Long.valueOf;
+
+public class GetOrdersHandler implements Route{
+    private OrderService orderService;
+    private Gson gson = new Gson();
+
+    public GetOrdersHandler(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        response.header("Content-Type", "application/json;charset=utf-8");
+        List<Order> orders = orderService.getOrders();
+        return gson.toJson(orders);
+    }
+
+
 }
+
+
