0321a1bf5d3a678842753931eecab3b9b2845427
Razvan Mihai <42058259+razvanm3@users.noreply.github.com>
2019-04-02 07:23:13 +0300

    Add files via upload

--- orders-service-client/src/main/java/http/GetOrderByIdClient.java
+++ orders-service-client/src/main/java/http/GetOrderByIdClient.java
@@ -4,10 +4,13 @@ import com.google.gson.Gson;
 import com.procourier.model.Order;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GetOrderByIdClient {
     private Gson gson = new Gson();
@@ -41,4 +44,36 @@ public class GetOrderByIdClient {
     }
 
 
+}
+
+public class GetOrdersClient {
+    private Gson gson = new Gson();
+    private HttpClient httpClient;
+
+    public GetOrdersClient(){
+        httpClient = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+    }
+
+    public List<Order> getOrders(){
+        Type OrderList = new TypeToken<ArrayList<Order>>(){
+        }.getType();
+
+        final HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("http://localhost:8082/orders"))
+                .build();
+
+        try {
+            final HttpResponse response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            return gson.fromJson(response.body().toString(), OrderList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
